119b1780fcac9b06e8fa87ae1f132f376b8f29ef
Alex Schoof <alex@luminal.io>
2015-04-22 09:13:16 -0400

    added classifiers to setup

--- setup.py
+++ setup.py
@@ -6,7 +6,13 @@ setup(
     description='A utility for managing secrets in the cloud using AWS KMS and DynamoDB',
     license='Apache2',
     url='https://github.com/LuminalOSS/credstash',
-
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: System :: Utilities'
+        ],
     install_requires=['boto>=2.38.0', 'pycrypto>=2.6.1'],
     scripts=['credstash.py'],
     entry_points={
