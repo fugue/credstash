62c98b13af42b9ee31918d8adc887f42b1d24061
Alex Schoof <alex@luminal.io>
2015-12-02 09:56:27 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='credstash',
-    version='1.7',
+    version='1.8',
     description='A utility for managing secrets in the cloud using AWS KMS and DynamoDB',
     license='Apache2',
     url='https://github.com/LuminalOSS/credstash',
