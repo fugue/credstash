4e06ac49078acc9b636ae8b3eff7ccaec413fc8c
Nathan Muir <ndmuir@gmail.com>
2016-09-28 11:43:59 +1000

    Cleanup `get_secret`

--- credsmash/api/get.py
+++ credsmash/api/get.py
@@ -7,22 +7,29 @@ from credsmash.util import ItemNotFound, padded_int
 
 def get_secret(secrets_table, key_service, secret_name, version=None):
     if version is None:
-        # do a consistent fetch of the credential with the highest version
-        response = secrets_table.query(
-            Limit=1,
-            ScanIndexForward=False,
-            ConsistentRead=True,
-            KeyConditionExpression=ConditionKey("name").eq(secret_name)
-        )
-        if response["Count"] == 0:
-            raise ItemNotFound("Item {'name': '%s'} couldn't be found." % secret_name)
-        material = response["Items"][0]
+        material = get_latest_secret(secrets_table, secret_name)
     else:
-        version = padded_int(version)
-        response = secrets_table.get_item(Key={"name": secret_name, "version": version})
-        if "Item" not in response:
-            raise ItemNotFound(
-                "Item {'name': '%s', 'version': '%s'} couldn't be found." % (secret_name, version))
-        material = response["Item"]
-
+        material = get_versioned_secret(secrets_table, secret_name, version)
     return open_aes_ctr_legacy(key_service, material)
+
+
+def get_latest_secret(secrets_table, secret_name):
+    # do a consistent fetch of the credential with the highest version
+    response = secrets_table.query(
+        Limit=1,
+        ScanIndexForward=False,
+        ConsistentRead=True,
+        KeyConditionExpression=ConditionKey("name").eq(secret_name)
+    )
+    if response["Count"] == 0:
+        raise ItemNotFound("Item {'name': '%s'} couldn't be found." % secret_name)
+    return response["Items"][0]
+
+
+def get_versioned_secret(secrets_table, secret_name, version):
+    version = padded_int(version)
+    response = secrets_table.get_item(Key={"name": secret_name, "version": version})
+    if "Item" not in response:
+        raise ItemNotFound(
+            "Item {'name': '%s', 'version': '%s'} couldn't be found." % (secret_name, version))
+    return response["Item"]
