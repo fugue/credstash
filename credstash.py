fcebcf71d1975d90afe410e8f315bd82326b948d
rhollister <r-github@hollisters.org>
2018-10-10 08:12:06 -0400

    allow binary secrets to be saved and returned

--- credstash.py
+++ credstash.py
@@ -140,6 +140,13 @@ def printStdErr(s):
     sys.stderr.write("\n")
 
 
+def printStdOut(s):
+    if hasattr(sys.stdout, 'buffer'):
+        sys.stdout.buffer.write(s)
+    else:
+        sys.stdout.write(binaryBuffer)
+
+
 def fatal(s):
     printStdErr(s)
     sys.exit(1)
@@ -167,23 +174,24 @@ def value_or_filename(string):
     # string to this function before it passes the actual value.
     # If an empty string is passes in, just return an empty string
     if string == "":
-        return ""
+        return "".encode()
 
+    output = ""
     if string == '-':
         try:
-            return sys.stdin.read()
+            output = sys.stdin.read().encode()
         except KeyboardInterrupt:
             raise argparse.ArgumentTypeError("Unable to read value from stdin")
     elif string[0] == "@":
         filename = string[1:]
         try:
-            with open(os.path.expanduser(filename)) as f:
+            with open(os.path.expanduser(filename), 'rb') as f:
                 output = f.read()
         except IOError:
             raise argparse.ArgumentTypeError("Unable to read file %s" %
                                              filename)
     else:
-        output = string
+        output = string.encode()
     return output
 
 
@@ -363,7 +371,7 @@ def getAllAction(args, region, **session_params):
     elif args.format == 'dotenv':
         output_func = dotenv_dump
         output_args = {}
-    print(output_func(secrets, **output_args))
+    printStdout(output_func(secrets, **output_args))
 
 
 @clean_fail
@@ -445,9 +453,9 @@ def getSecretAction(args, region, **session_params):
             elif args.format == 'dotenv':
                 output_func = dotenv_dump
                 output_args = {}
-            sys.stdout.write(output_func(secrets, **output_args))
+            printStdout(output_func(secrets, **output_args))
         else:
-            sys.stdout.write(getSecret(args.credential, args.version,
+            printStdout(getSecret(args.credential, args.version,
                                        region=region, table=args.table,
                                        context=args.context,
                                        **session_params))
@@ -600,7 +608,7 @@ def open_aes_ctr_legacy(key_service, material):
         hmac = codecs.decode(material['hmac'].value, "hex")
     else:
         hmac = codecs.decode(material['hmac'], "hex")
-    return _open_aes_ctr(key, LEGACY_NONCE, ciphertext, hmac, digest_method).decode("utf-8")
+    return _open_aes_ctr(key, LEGACY_NONCE, ciphertext, hmac, digest_method)
 
 
 def seal_aes_ctr_legacy(key_service, secret, digest_method=DEFAULT_DIGEST):
@@ -637,7 +645,7 @@ def _open_aes_ctr(key, nonce, ciphertext, expected_hmac, digest_method):
     return decryptor.update(ciphertext) + decryptor.finalize()
 
 
-def _seal_aes_ctr(plaintext, key, nonce, digest_method):
+def _seal_aes_ctr(data, key, nonce, digest_method):
     data_key, hmac_key = _halve_key(key)
     encryptor = Cipher(
         algorithms.AES(data_key),
@@ -645,7 +653,7 @@ def _seal_aes_ctr(plaintext, key, nonce, digest_method):
         backend=default_backend()
     ).encryptor()
 
-    ciphertext = encryptor.update(plaintext.encode("utf-8")) + encryptor.finalize()
+    ciphertext = encryptor.update(data) + encryptor.finalize()
     return ciphertext, _get_hmac(hmac_key, ciphertext, digest_method)
 
 
