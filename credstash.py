9fcc027666bbfea97a1deb8a8050e126f23919a7
John Corrales <jjcorrales3@gmail.com>
2019-04-03 09:12:52 -0700

    add type to param

--- credstash.py
+++ credstash.py
@@ -911,7 +911,7 @@ def get_parser():
     action = 'setup'
     parsers[action] = subparsers.add_parser(action,
                                             help='setup the credential store')
-    parsers[action].add_argument("--tags",
+    parsers[action].add_argument("--tags", type=key_value_pair,
                                   help="Tags to apply to the Dynamodb Table"
                                   "passed in as a space sparated list of Key=Value", nargs="*")
     parsers[action].set_defaults(action=action)
